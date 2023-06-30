61e340cab9fd3a505ff783de89cebcf04e64a2d6
Nicolas <nicolas.velez99@hotmail.com>
2023-06-30 18:46:40 -0500

    [feat]: controlador del registro

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class RegisterController extends Controller
+{
+
+    public function index()
+    {
+        return view('register.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+
+        return redirect()->route('login')->with('success', 'Registro exitoso. Por favor, inicia sesión.');
+    }
+}
