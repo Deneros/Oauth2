86b2cc561850f5e8b1b48dd1436699cb339d8dbb
Nicolas <nicolas.velez99@hotmail.com>
2023-06-30 23:26:26 -0500

    [fix]: se cambian campos de lugar

--- database/migrations/2023_10_12_000000_create_users_table.php
+++ database/migrations/2023_10_12_000000_create_users_table.php
@@ -16,18 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('family_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('gender_id')->nullable();
-            $table->unsignedBigInteger('housing_type_id')->nullable();
-            $table->unsignedBigInteger('identification_type_id')->nullable();
+            $table->rememberToken(); 
             $table->timestamps();
-
-            $table->foreign('gender_id')->references('id')->on('genders');
-            $table->foreign('housing_type_id')->references('id')->on('housing_types');
-            $table->foreign('identification_type_id')->references('id')->on('identification_types');
         });
     }
 
---
+++ database/migrations/2023_11_30_202343_create_form_data.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('form_data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('identification_type_id')->unique();
+            $table->foreign('identification_type_id')->references('id')->on('identification_types')->onDelete('cascade');
+            $table->unsignedBigInteger('gender_id')->nullable();
+            $table->foreign('gender_id')->references('id')->on('genders')->onDelete('cascade');
+            $table->unsignedBigInteger('housing_type_id')->nullable();
+            $table->foreign('housing_type_id')->references('id')->on('housing_types')->onDelete('cascade');
+            $table->date('date_of_birth');
+            $table->string('city_of_birth');
+            $table->string('nationality');
+            $table->string('residence_address');
+            $table->string('neighborhood');
+            $table->string('place_of_residence');
+            $table->string('cellphone');
+            $table->integer('dependents')->nullable();
+            $table->boolean('has_children');
+            $table->integer('number_of_children')->nullable();
+            $table->integer('children_living_with_you')->nullable();
+            $table->integer('adult_children')->nullable();
+            $table->boolean('voted_2022_congress_presidency')->nullable();
+            $table->boolean('voted_2019_mayor_governor')->nullable();
+            $table->boolean('registered_id_dagua')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('form_data');
+    }
+};
