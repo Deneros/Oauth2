a023724164dc3fba808c8d0d92455f2279930a42
Nicolas <nicolas.velez99@hotmail.com>
2023-07-11 14:48:18 -0500

    [feat]: seeders para roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(HousingTypeSeeder::class);
         $this->call(TopicSeeder::class);
         $this->call(CitiesSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
