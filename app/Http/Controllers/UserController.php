86c948bad739bc9145245638daa0a903fa75f14a
Nicolas <nicolas.velez99@hotmail.com>
2023-07-12 15:05:43 -0500

    [feat]: se arreglan los roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,38 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Role;
 
 class UserController extends Controller
 {
-   
+
     public function index()
     {
-        $users = User::all();
+        $users = User::with('role')->get();
+        $roles = Role::pluck('name', 'id');
 
-        return view('users.index', compact('users'));
+        return view('users.index', compact('users', 'roles'));
     }
 
-
- 
-  
-    // public function editRole(User $user)
-    // {
-    //     // Obtener los roles disponibles desde tu modelo de roles
-    //     $roles = Role::all();
-
-    //     return view('users.edit_role', compact('user', 'roles'));
-    // }
-
-
-    public function update(Request $request, User $user)
+    public function editRole(int $user)
     {
-        
+
+        $roles = Role::pluck('name', 'id');
+        $user = User::findOrFail($user);
+
+        return view('users.edit_role', compact('user', 'roles'));
     }
 
- 
+
+    public function update(Request $request, int $user)
+    {
+        $request->validate([
+            'role_id' => 'required|exists:roles,id',
+        ]);
+        $user = User::findOrFail($user);
+
+        $user->role_id = $request->input('role_id');
+        $user->save();
+
+        return redirect('/backstage')->with('success', 'El rol del usuario se actualizó correctamente.');
+    }
+
+
     public function destroy(User $user)
     {
         //
