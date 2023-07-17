854fa4ff0d88484bfee4fc52129e2ddc1828aa6e
Nicolas <nicolas.velez99@hotmail.com>
2023-07-17 03:41:33 -0500

    [feat]: rutas para los controladores

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MeetingController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +22,32 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'auth'])->name('login.auth');
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/register/{role?}', [RegisterController::class, 'index'])->name('register');
 Route::post('/register/save', [RegisterController::class, 'register'])->name('register.save');
 Route::get('/login/google', [LoginController::class, 'redirectToGoogle'])->name('login.google');
 Route::get('/login/google/callback', [LoginController::class, 'handleGoogleCallback']);
 
-Route::get('/backstage', [UserController::class, 'index']);
+
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/meetings', [MeetingController::class, 'index'])->name('meetings.index');
+    Route::post('/create-meeting', [MeetingController::class, 'store'])->name('meetings.store');
+    Route::post('/meetings/{id}/mark-completed', [MeetingController::class, 'markCompleted'])->name('meetings.markCompleted');
+    Route::post('/meetings/{meeting}/attach-document', [MeetingController::class, 'attachDocument'])->name('meetings.attachDocument');
+    Route::get('meetings/{id}/download', [MeetingController::class, 'downloadDocument'])->name('meetings.downloadDocument');
+
+    Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+    Route::post('/reports/generate', [ReportController::class, 'generate'])->name('reports.generate');
+
+
     Route::get('/form', [FormController::class, 'index'])->name('form.index');
     Route::post('/form', [FormController::class, 'store'])->name('form.store');
+
+    Route::get('/backstage', [UserController::class, 'index'])->name('backstage.user');
+    Route::get('/users-edit/{user}', [UserController::class, 'editRole'])->name('users.edit_role');
+    Route::put('/users-update/{user}', [UserController::class, 'update'])->name('users.update_role');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home.index');
 });
